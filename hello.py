9eebdfe6c25ea147653fc2f10495e5000ff22d49
hanyujun <hanyujun@hanyujundeMacBook-Air.local>
2015-06-07 00:01:30 +0800

    anthor debug in bt

--- hello.py
+++ hello.py
@@ -1,12 +1,9 @@
 #! usr/bin/env python
 #-*-coding:utf-8-*-
 print u'中文'
-import ipdb
-ipdb.set_trace()
 a = raw_input('please input the data')
 print a 
 print 'hello %s ,you  have  $%d' %('jack',100000)
-ipdb.set_trace()
 print ord('a')
 print chr(65)
 def action(x):
