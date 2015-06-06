0e70b805817dceb9906d6aa8952daf483218b06c
hanyujun <hanyujun@hanyujundeMacBook-Air.local>
2015-06-06 23:56:10 +0800

    another debug test

--- hello.py
+++ hello.py
@@ -6,6 +6,8 @@ ipdb.set_trace()
 a = raw_input('please input the data')
 print a 
 print 'hello %s ,you  have  $%d' %('jack',100000)
+import ipdb
+ipdb.set_trace()
 print ord('a')
 print chr(65)
 def action(x):
