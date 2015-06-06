9aa471df4ac9ae4a1d58dd5dc1f0e7b727e25d55
hanyujun <hanyujun@hanyujundeMacBook-Air.local>
2015-06-06 23:43:45 +0800

    test how to debug in diffrent branch

---
+++ hello.py
@@ -0,0 +1,14 @@
+#! usr/bin/env python
+#-*-coding:utf-8-*-
+print u'中文'
+import ipdb
+ipdb.set_trace()
+a = raw_input('please input the data')
+print a 
+print 'hello %s ,you  have  $%d' %('jack',100000)
+print ord('a')
+print chr(65)
+def action(x):
+    return lambda y:x+y
+str1 = 'test how to divide the screen'
+
