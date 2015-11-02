c4a7f686794993ab4fd59bc517986443c84dc9c5
hanyujun <hanyujun@hichao.com>
2015-11-02 15:32:40 +0800

    修改polynonim.py

--- polynonim.py
+++ polynonim.py
@@ -7,6 +7,8 @@ import re
 import string
 import time
 from datetime import datetime
+import requests
+import json
 TEL_REG = r"(13|14|15|17|18)\d{9}$"
 class Animal(object):
  def run(self):
@@ -47,5 +49,36 @@ def test_time():
         hour = 23
     publish_date = int(time.mktime(datetime(now.tm_year, now.tm_mon, now.tm_mday, hour, 0, 0).timetuple()))
     print publish_date
+
+def get_customer_online_state(business_ids):
+    
+    url = 'http://v1.platform.chat.hicao.com/cs/chat.cgi' 
+    headers = { 
+        'Cookie':'PHPSESSID=ed012b9vt17gg1986aod1l6et1',
+    }   
+    #str_business_ids = ','.join(business_ids)
+    str_business_ids = '745,11,765,803,747'
+    query = {}
+    query["version"] = "2.0"
+    query["message_type"] = 70
+    query["business_id_list"] = str_business_ids
+    print str_business_ids
+    isOnline = 0 
+    business_id2online_state = {business_id:isOnline for business_id in business_ids}
+    res = requests.post(url,data="data:"+json.dumps(query),headers=headers,verify=True)
+    print res
+    result = res.json()
+    if result.has_key("state_list"):
+        state_list = result['state_list']
+        for bid, current_platform in state_list.items():
+            is_online_curr = 0 
+            for item in current_platform['platform']:
+                if item["state"] == 0:
+                    is_online_curr = 1 
+                    break
+            business_id2online_state[str(bid)] = is_online_curr
+    return business_id2online_state
+
 if __name__=='__main__':
-    test_time()
+    business_ids = [745,11,765,803,74]
+    get_customer_online_state(business_ids)
